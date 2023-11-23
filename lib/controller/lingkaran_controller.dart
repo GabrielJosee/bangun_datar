f71c7f74222c8de6cc32d95fe18cc34d1d412aad
GabrielJosee <145422492+GabrielJosee@users.noreply.github.com>
2023-11-23 15:03:08 +0700

    project akhir

--- lib/controller/lingkaran_controller.dart
+++ lib/controller/lingkaran_controller.dart
@@ -6,15 +6,24 @@ class LingkaranController extends GetxController{
   final angka = "".obs;
 
   void hitungluas(){
-    var hitungL = 3.14 * r * r;
-    hasil.value = "Hasil Perhitungan luas dari jari jari $r adalah $hitungL";
-    angka.value = "$hitungL";
+    var hitung;
+    if(r % 7 == 0){
+      hitung = (22 / 7) * r * r;
+    }else{
+      hitung = 3.14 * r * r;
+    }
+    hasil.value = "Hasil Perhitungan luas dari jari jari $r adalah $hitung";
+    angka.value = "$hitung";
   }
 
   void hitungkeliling(){
-    var hitungK = 2 * 3.14 * r;
-    hasil.value = "Hasil Perhitungan Keliling dari jari jari $r adalah $hitungK";
-    angka.value = "$hitungK";
-  }
-
+    var keliling;
+    if(r % 7 == 0){
+      keliling = 2 *  (22 / 7) * r;
+    }else{
+      keliling = 2 * 3.14 * r;
+    }
+    hasil.value = "Hasil Perhitungan Keliling dari jari jari $r adalah $keliling";
+    angka.value = "$keliling";
+    }
 }
